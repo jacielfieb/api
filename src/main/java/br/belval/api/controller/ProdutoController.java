68d91e5e272129b7a73be5ea5a26ede0093429ac
Jaciel <rm101673@estudante.fieb.edu.br>
2026-05-08 22:14:28 -0300

    projeto api funcionando salva,pesquisar excluir

--- src/main/java/br/belval/api/controller/ProdutoController.java
+++ src/main/java/br/belval/api/controller/ProdutoController.java
@@ -1,12 +1,16 @@
 package br.belval.api.controller;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,6 +54,70 @@ public class ProdutoController {
 		return ResponseEntity
 				.status(HttpStatus.CREATED)
 				.body(produto);
+	}
+	
+	@GetMapping("/produtos/{id}")
+	public ResponseEntity<Object> buscarPorId(
+			@PathVariable(value = "id") Integer id){
+		
+		Optional<Produto> produtoOpt = repository.findById(id);
+		
+		if(produtoOpt.isPresent()) {
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(produtoOpt.get());				
+		}
+		
+		return ResponseEntity
+				.status(HttpStatus.NOT_FOUND)
+				.body("produto não encontrado");	
+		
+	}
+	
+	
+	
+	
+	//curl -X PUT http://localhost:8080/produtos/1 -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao2.json
+	@PutMapping("/produtos/{id}")
+	public ResponseEntity<Object> atualizaProduto(
+			
+			@PathVariable Integer id,
+			@RequestBody Produto produto){
+		
+		Optional<Produto> produtoOpt = repository.findById(id);
+		
+		if(produtoOpt.isEmpty()) {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.body("produto não encontrado!");				
+		}
+		
+		produto.setId(id);
+		produto.setDataCriacao(produtoOpt.get().getDataCriacao());
+				repository.save(produto);
+				return ResponseEntity
+						.status(HttpStatus.OK)
+						.body("produto atualizado com sucesso");
+		
+	}
+	
+	
+	@DeleteMapping("/produtos/{id}")
+	public ResponseEntity<String> apagarProduto(@PathVariable Integer id) {
+
+	    Optional<Produto> produtoOpt = repository.findById(id);
+
+	    if (produtoOpt.isEmpty()) {
+	        return ResponseEntity
+	                .status(HttpStatus.NOT_FOUND)
+	                .body("Produto não encontrado!");
+	    }
+
+	    repository.deleteById(id);
+
+	    return ResponseEntity
+	            .status(HttpStatus.OK)
+	            .body("Produto apagado com sucesso!");
 	}
 
 }
--- src/main/java/br/belval/api/repository/ProdutoRepository.java
+++ src/main/java/br/belval/api/repository/ProdutoRepository.java
@@ -10,7 +10,7 @@ import br.belval.api.model.Produto;
 public interface ProdutoRepository extends CrudRepository <Produto, Integer>{
 	
 	/*List<Produto> findByNomeContainingOrDescricaoContaining(String texto2);*/
-	List<Produto> findByNomeContainingOrDescricaoContaining(String nome,String descricao);
+	List<Produto> findByNomeContainingOrDescricaoContaining(String descricao,String nome);
 	
 }
 
